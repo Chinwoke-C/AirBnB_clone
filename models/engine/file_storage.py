ec36e1c695bc0005a7f9835cd42970618dce300f
Chinwoke <canugwara99@gmail.com>
2024-02-11 01:03:09 -0800

    updated models and engine

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-""" """
+"""Defines a FileStorage class"""
 
 
 
@@ -8,33 +8,35 @@ import json
 from os.path import exists
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
-    """ """
+    """Represents an abstracted storage engine"""
 
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """ """
+        """Return the dictionary __objects."""
         return self.__objects
     
     def new(self, obj):
-        """ """
+        """Set in __objects obj with key <obj_class_name>.id"""
         class_name = obj.__class__.__name__
         self.__objects[f"{class_name}.{obj.id}"] = obj
 
     
     def save(self):
-        """ """
-        obj_dict = {}
-        for key, values in FileStorage.__objects.items():
-            # I dont know whats causing the error on this line
-            # I've tried everything
-            obj_dict[key] = values.to_dict()
-        with open(FileStorage.__file_path, "w", encoding='utf-8') as file:
-            json.dump(obj_dict, file)
+          """ serializes objects to the JSON file (path: __file_path) """
+        with open(FileStorage.__file_path, 'w', encoding='utf-8') as fname:
+            new_dict = {key: obj.to_dict() for key, obj in
+                        FileStorage.__objects.items()}
+            json.dump(new_dict, fname)
 
 
 # I used this to handle error IGNORE!!!!
